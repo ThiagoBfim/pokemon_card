1ad49ccfbad76be635a3f4a46bc09e5f08e2fc3e
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-09-19 08:49:03 -0300

    Criação da lista

---
+++ lib/app/modules/home/domain/pokemon.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class Pokemon {
+
+  final String id;
+  final List<dynamic> types;
+  final String imageUrl;
+  final String imageUrlHiRes;
+  final String name;
+
+  Pokemon({
+    @required this.id,
+    @required this.types,
+    @required this.imageUrl,
+    @required this.imageUrlHiRes,
+    @required this.name});
+
+}
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokemon_dio/app/modules/home/domain/pokemon.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -13,15 +11,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeController> {
   //use 'controller' variable to access controller
 
+  final List<Pokemon> pokemons = [
+    Pokemon(
+        id: 'xy7-10',
+        types: ["Lightning"],
+        imageUrl: "https://images.pokemontcg.io/ex16/16.png",
+        imageUrlHiRes: "https://images.pokemontcg.io/ex16/16_hires.png",
+        name: "Magneton"),
+    Pokemon(
+      id: 'pl2-103',
+      types: ["Psychic"],
+      imageUrl: "https://images.pokemontcg.io/pl2/103.png",
+      name: "Alakazam",
+      imageUrlHiRes: "https://images.pokemontcg.io/pl2/103_hires.png",
+    ),
+    Pokemon(
+        id: 'ex8-100',
+        types: ["Fighting"],
+        imageUrl: "https://images.pokemontcg.io/ex8/100.png",
+        imageUrlHiRes: "https://images.pokemontcg.io/ex8/100_hires.png",
+        name: "Hariyama")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
-    );
+        body: GridView.count(
+      childAspectRatio: 0.72,
+      crossAxisCount: 2,
+      children: List.generate(pokemons.length, (index) {
+        var pokemon = pokemons[index];
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 5.0),
+          child: Hero(
+            tag: pokemon.id,
+            child: Image.network(pokemon.imageUrl),
+          ),
+        );
+      }),
+    ));
   }
 }
